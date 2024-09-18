61d75ceef337fefbbf74f48b1bd937eb6c0d106a
Sinless Rook <142495840+SinlessRook@users.noreply.github.com>
2024-09-18 22:49:40 +0530

    Update Hello_World.c

--- Module 1/Hello_World.c
+++ Module 1/Hello_World.c
@@ -1,6 +1,17 @@
+//Algorithm
+1. Start
+2. Define a function `main` with a return type `void`.
+3. Inside the `main` function:
+   a. Display the message "Hello World!!" on the screen.
+4. End
+
+//Code
 #include<stdio.h>
 
 void main()
 {
   printf("Hello World!!\n");
 }
+
+//Output
+Hello World!!
