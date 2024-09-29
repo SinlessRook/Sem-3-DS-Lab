aabf85c6bad77dcff17c76b5a6920a9ae855b1f7
Adithyan A S <142495840+SinlessRook@users.noreply.github.com>
2024-09-29 22:40:12 +0530

    Update Postfix_Evaluvation.c

--- Module 2/Postfix_Evaluvation.c
+++ Module 2/Postfix_Evaluvation.c
@@ -6,36 +6,39 @@ int MAX_SIZE=40;
 int stk[40];
 int top=-1;
 
+//Stack Operations
 void push(int item);
 int pop();
 int peek();
 int isfull();
 int isempty();
-int isOperator(char ch);
-int performOperation(int item1,int item2,char ch);
+
+int isOperator(char ch);//Check whether a charactor is operator using if else statement
+int performOperation(int item1,int item2,char ch);//Perform The required evaluvation of a simple expression
 int main() {
     int choice=0;
     char string[10];
     int item1,item2;
     while(choice!=4)
     {
-        printf("Enter the postfix expression:");
+        printf("Enter the postfix expression:");//Input From user
         scanf("%s",&string);
-        for(int i=0;string[i]!='\0';i++)
+        for(int i=0;string[i]!='\0';i++)//Traversing each character until null character is reached
         {
             if(isOperator(string[i])==1)
             {
-                
+                //If charactor encountered is operator popping two elements and evaluvating expression
                 item1 = pop();
                 item2 = pop();
                 push(performOperation(item1,item2,string[i]));
             }
             else
             {
-                push(string[i]-'0');
+                //If character is operand push it to stack
+                push(string[i]-'0');//Converting type of character from char to int
             }
         }
-        printf("Result:%d\n",pop());
+        printf("Result:%d\n",pop());//Printing the last element in stack(result)
         printf("To exit enter 4:");
         scanf("%d",&choice);
     }
@@ -57,6 +60,7 @@ int isOperator(char ch)
 
 int performOperation(int item1,int item2,char ch)
 {
+    //Evaluvating expression based on type of operator
     switch(ch)
     {
         case '+':
