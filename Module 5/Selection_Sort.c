04100b609555e3c1c2d68130b34f235705987312
Adithyan A S <142495840+SinlessRook@users.noreply.github.com>
2024-10-24 19:35:12 +0530

    Update Selection_Sort.c

--- Module 5/Selection_Sort.c
+++ Module 5/Selection_Sort.c
@@ -24,7 +24,7 @@ int main()
 void SelectionSort(int lst[],int n,int lst_new[])
 {
     int min;;
-    for(int i=0;i<n;i++)
+    for(int i=0;i<n-1;i++)
     {
         min = lst[i];
         for(int j=i+1;j<n;j++)
